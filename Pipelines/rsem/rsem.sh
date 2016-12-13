88c09d97349e354faa989149e79ab4994dcf5890
mbelmadani <manuel.belmadani@gmail.com>
2016-12-13 00:08:16 -0800

    Workaround rsem issue with --paired-end changing the order of parameters.

--- Pipelines/rsem/rsem.sh
+++ Pipelines/rsem/rsem.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+set -eu
+source ../../etc/load_configs.sh
 
-RSEM_EXE="/misc/pipeline42/NeuroGem/pipeline/RSEM/bin/rsem-calculate-expression"
-STAR="/space/bin/STAR-STAR_2.4.0h/bin/Linux_x86_64_static"
-#REFERENCE="/misc/pipeline42/NeuroGem/pipeline/runtime/human_ref38/human_0"
-REFERENCE="/misc/pipeline42/NeuroGem/pipeline/runtime/mouse_ref38/mouse_0"
+if [ $# -eq 0 ]
+    then
+    SERIES="GSE123456"
+    OUTPUT="quantified/$SERIES/SAMPLE1.fasrq.gz"
+    echo "Usage:"
+    echo $0" $SERIES SAMPLE1.fastq.gz <Optional, SAMPLE2.fastq.gz>"    
+    exit
+fi
 
-SAMPLE=$(basename $1)
-OUTPUT="quantified/$SAMPLE"
-TMP="temporary/$SAMPLE"
+RSEM_EXE="$RSEM_DIR/rsem-calculate-expression"
+REFERENCE=$STAR_DEFAULT_REFERENCE
+REFERENCE="/misc/pipeline42/NeuroGem/pipeline/runtime/mouse_ref38/mouse_0" 
+
+SERIES=$1
+SAMPLE=$(basename $2)
+
+OUTPUT="quantified/$SERIES/$SAMPLE"
+TMP="temporary/$SERIES/$SAMPLE"
 
 
 mkdir -p $OUTPUT
@@ -19,26 +31,42 @@ if [ $# -eq 0 ]
     exit
 fi
 
-if [ $# -gt 0 ]
-    then
-    ARGUMENTS="<(zcat -f $1)"
-fi
+PAIRED_END=""
+PAIR=""
 
-if [ $# -eq 0 ]
-    then
-    echo "Paired-end version not implemented."
-    exit
-fi
-
-#    <(zcat -f $@) \
-#    <(zcat -f $1) <(zcat -f /dev/null) \
-#    --paired-end \
-$RSEM_EXE \
-    -p 16 \
+CMD=$(echo $RSEM_EXE \
+    -p 1 \
+    --star-gzipped-read-file \
     --temporary-folder $TMP \
     --time \
     --star \
-    --star-path $STAR \
-    <(zcat -f $@) \
+    --star-path $STAR_PATH \
+    "$2" \
+    $OUTPUT \
     $REFERENCE \
-    $OUTPUT   
+    "--star-shared-memory LoadAndKeep" 
+)
+
+if [ $# -gt 2 ]
+    then
+    PAIRED_END=" --paired-end "
+    PAIR=$3
+
+    ## For some reason, the order of the parameters change for paired-end data.
+    CMD=$(echo $RSEM_EXE \
+    -p 1 \
+    --star-gzipped-read-file \
+    --temporary-folder $TMP \
+    --time \
+    --star \
+    --star-path $STAR_PATH \
+    "$2 $PAIR" \
+    $REFERENCE \
+    $OUTPUT \
+    "--star-shared-memory LoadAndKeep" \
+    $PAIRED_END
+)
+fi
+
+echo $CMD
+$CMD
