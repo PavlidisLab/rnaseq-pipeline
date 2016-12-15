0e2adbceb11e737ed84be450d794b2f24fa7c645
mbelmadani <manuel.belmadani@gmail.com>
2016-12-14 16:03:36 -0800

    Fixed paired-end and non-paired-end methods using the forked RSEM version.

--- Pipelines/rsem/multiple_rsem.sh
+++ Pipelines/rsem/multiple_rsem.sh
@@ -2,10 +2,35 @@
 set -eu
 source ../../etc/load_configs.sh
 
+if [ $# -eq 0 ]
+    then
+    FILES="data/GSE123456/"
+    SERIES="GSE123456"
+    echo "Description: "
+    echo "Provide a directory where sample runs are contained by distinct directories."
+    echo "Usage:"
+    echo "$0 <FILES> <SERIES>"
+    echo "$0 <FILES> <SERIES> --paired-end"
+    echo "Example:"
+    echo "$0 $FILES $SERIES"
+    echo "   where $FILES would have each sample under it's own directory."
+    exit
+fi
+MATES=""
+if [ $# -eq 3 ]
+then
+    MATES=1
+else
+    if [ $# -gt 3 ]
+	echo "Too many arguments! Please check your command."
+	exit -1
+    fi
+fi
+
 FILES=$1
 SERIES=$2
 REFERENCE=$(dirname $STAR_DEFAULT_REFERENCE)
-#NCPU=3 # Set to 2 because limited memory.
+NCPU=1 # Set to 2 because limited memory.
 
 echo "Preparing memory..."
 $RSEM_DIR/rsem-star-load-shmem $STAR_EXE $REFERENCE $NCPU #/misc/pipeline42/NeuroGem/pipeline/runtime/human_ref38/
--- Pipelines/rsem/rsem.sh
+++ Pipelines/rsem/rsem.sh
@@ -5,7 +5,7 @@ source ../../etc/load_configs.sh
 if [ $# -eq 0 ]
     then
     SERIES="GSE123456"
-    OUTPUT="quantified/$SERIES/SAMPLE1.fasrq.gz"
+    OUTPUT="quantified/$SERIES/SAMPLE1.fastq.gz"
     echo "Usage:"
     echo $0" $SERIES SAMPLE1.fastq.gz <Optional, SAMPLE2.fastq.gz>"    
     exit
@@ -16,57 +16,50 @@ REFERENCE=$STAR_DEFAULT_REFERENCE
 REFERENCE="/misc/pipeline42/NeuroGem/pipeline/runtime/mouse_ref38/mouse_0" 
 
 SERIES=$1
-SAMPLE=$(basename $2)
+SAMPLE=$(echo $(basename $2) | sed 's/.fastq.gz//g')
 
 OUTPUT="quantified/$SERIES/$SAMPLE"
 TMP="temporary/$SERIES/$SAMPLE"
 
-
 mkdir -p $OUTPUT
 mkdir -p $TMP
 
-if [ $# -eq 0 ]
-    then
-    echo "No arguments supplied. Please specify one or two (for paired-end data) fastq file(s)."
-    exit
-fi
-
+SEQUENCES=$2
 PAIRED_END=""
-PAIR=""
+MATE=""
 
-#    --temporary-folder $TMP \
-CMD=$(echo $RSEM_EXE \
-    -p 1 \
-    --star-gzipped-read-file \
-    "$2" \
-    --time \
-    --star \
-    --star-path $STAR_PATH \
-    --temporary-folder $TMP \
-    $OUTPUT \
-    $REFERENCE \
-    "--star-shared-memory LoadAndKeep"
-)
 
-if [ $# -gt 2 ]
-    then
+if [ $# -gt 3 ] 
+then
     PAIRED_END=" --paired-end "
-    PAIR=$3
+    MATES=$3
 
-    ## For some reason, the order of the parameters change for paired-end data.
     CMD=$(echo $RSEM_EXE \
-    -p 1 \
-    --star-gzipped-read-file \
-    --temporary-folder $TMP \
-    --time \
-    --star \
-    --star-path $STAR_PATH \
-    "$2 $PAIR" \
-    $REFERENCE \
-    $OUTPUT \
-    "--star-shared-memory LoadAndKeep" \
-    $PAIRED_END
+	-p 8 \
+	--star-gzipped-read-file \
+	--temporary-folder $TMP \
+	--time \
+	--star \
+	--star-path $STAR_PATH \
+	$PAIRED_END \
+	" $SEQUENCES $MATE " \
+	$REFERENCE \
+	$OUTPUT \
+	"--star-shared-memory LoadAndKeep" \
 )
+else
+    CMD=$(echo $RSEM_EXE \
+	-p 8 \
+	--star-gzipped-read-file \
+	--time \
+	--star \
+	--star-path $STAR_PATH \
+	--temporary-folder $TMP \
+	" $SEQUENCES " \
+	$REFERENCE \
+	$OUTPUT \
+	"--star-shared-memory LoadAndKeep"
+    )   
 fi
 
 echo $CMD >> log.txt
