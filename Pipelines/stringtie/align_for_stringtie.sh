3667d9d8085fae5012b981972426453bd91fc105
mbelmadani <manuel.belmadani@gmail.com>
2017-02-14 21:32:24 -0800

    Added stringtie pipeine (stub)

---
+++ Pipelines/stringtie/align_for_stringtie.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -eu
+source ../../etc/load_configs.sh
+
+if [ $# -eq 0 ]
+    then
+    SERIES="GSE123456"
+    echo "Usage:"
+    echo $0" $SERIES SEQUENCES1.fastq.gz,SEQUENCES2.fastq.gz... <Optional, SEQUENCES1_MATE.fastq.gz,SEQUENCES2_MATE.fastq.gz...>"    
+    return 1
+fi
+
+&>2 echo "Launching: -->" $0 $@ &1>2
+
+SERIES=$1
+while [[ $SERIES == */ ]]; do
+    # Cleaning up "$SERIES"
+    SERIES=$(echo $SERIES | sed 's|/$||g')
+    echo "WARNING: Please do not use trailing forward-slashes in $SERIES. Removing it..." 
+done 
+
+shift
+FASTQ_GZ=$@
+
+# Using aligner $STAR_EXE
+
+STAR_GENOMEDIR=$(dirname $STAR_DEFAULT_REFERENCE) # TODO: Add parameter for this
+
+$STAR_EXE \
+    --limitBAMsortRAM $STAR_MAX_BAM_RAM \
+    --genomeDir $STAR_GENOMEDIR  \
+    --outSAMunmapped Within \
+    --outFilterType BySJout \
+    --outSAMattributes NH HI AS NM MD \
+    --outFilterMultimapNmax 20 \
+    --outFilterMismatchNmax 999 \
+    --outFilterMismatchNoverLmax 0.04 \
+    --alignIntronMin 20 \
+    --alignIntronMax 1000000 \
+    --alignMatesGapMax 1000000  \
+    --alignSJoverhangMin 8  \
+    --alignSJDBoverhangMin 1  \
+    --sjdbScore 1  \
+    --runThreadN $NCPU_NICE  \
+    --genomeLoad LoadAndKeep  \
+    --outSAMtype BAM SortedByCoordinate  \
+    --quantMode TranscriptomeSAM  \
+    --outSAMheaderHD \@HD VN:1.4 SO:unsorted  \
+    --readFilesCommand zcat  \
+    --readFilesIn $FASTQ_GZ  \
+    --outStd BAM_SortedByCoordinate
+
+    #--outFileNamePrefix temporary/$SERIES/$SAMPLE \
+# /space/bin/STAR-STAR_2.4.0h/bin/Linux_x86_64_static/STAR --genomeDir /misc/pipeline42/mbelmadani/rnaseq-pipeline/Assemblies/runtime/human_ref38  --outSAMunmapped Within  --outFilterType BySJout  --outSAMattributes NH HI AS NM MD  --outFilterMultimapNmax 20  --outFilterMismatchNmax 999  --outFilterMismatchNoverLmax 0.04  --alignIntronMin 20  --alignIntronMax 1000000  --alignMatesGapMax 1000000  --alignSJoverhangMin 8  --alignSJDBoverhangMin 1  --sjdbScore 1  --runThreadN 4  --genomeLoad LoadAndKeep  --outSAMtype BAM Unsorted  --quantMode TranscriptomeSAM  --outSAMheaderHD \@HD VN:1.4 SO:unsorted  --outFileNamePrefix temporary/I16R008_Neurogem/I16R008b06/I16R008b06  --readFilesCommand zcat  --readFilesIn /misc/pipeline42/NeuroGem/data/PD/raw_data//I16R008_Neurogem/I16R008b06/I16R008b06_01_R1.fastq.gz
---
+++ Pipelines/stringtie/stringtie.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -eu
+source ../../etc/load_configs.sh
+
+if [ $# -eq 0 ]
+    then
+    SERIES="GSE123456"
+    echo "Usage:"
+    echo $0" $SERIES SEQUENCES1.fastq.gz,SEQUENCES2.fastq.gz... <Optional, SEQUENCES1_MATE.fastq.gz,SEQUENCES2_MATE.fastq.gz...>"    
+    exit
+fi
+echo "Launching: -->" $0 $@
+
+
+# SAMPLE=$(echo $(basename $2) | sed 's/,.*//g' | sed 's/.fastq.gz//g')
+# SAMPLE=$(echo $2 | sed "s|.*$SERIES|$SERIES|g" | sed  "s|.*$SERIES\/?\(.*\)|\\1|g" | sed "s|\/.*||" ) 
+SERIES=$1
+while [[ $SERIES == */ ]]; do
+    # Cleaning up "$SERIES"
+    SERIES=$(echo $SERIES | sed 's|/$||g')
+    echo "WARNING: Please do not use trailing forward-slashes in $SERIES. Removing it..." 
+done 
+
+SAMPLE=$(echo $2 | sed "s|.*$SERIES\/|\/|g" | sed "s|\/\/|\/|g" | cut -d"/" -f2) # Grab whatever trails $SERIES until the next forward slash.
+echo "SampleID: $SAMPLE"
+
+OUTPUT="quantified/$SERIES/$SAMPLE"
+TMP="temporary/$SERIES/$SAMPLE"
+
+mkdir -p $OUTPUT
+mkdir -p $TMP
+
+PAIRED_END=""
+MATE=""
+
+echo "Sequence type:"
+if [ $# -gt 9999999999 ] 
+then
+    echo "Not implemented"
+    return 1
+    exit
+    
+    echo " Paired end sequences. "
+    PAIRED_END=" --paired-end "
+    MATES=$3
+
+    CMD=$(echo $RSEM_EXE \
+	-p "$NCPU_NICE" \
+	--star-gzipped-read-file \
+	--temporary-folder $TMP \
+	--time \
+	--star \
+	--star-path $STAR_PATH \
+	$PAIRED_END \
+	" $SEQUENCES $MATE " \
+	$REFERENCE \
+	$OUTPUT \
+	"--star-shared-memory LoadAndKeep" \
+)
+else
+    echo " Single-end sequences!"
+    CMD=$( ./align_for_stringtie.sh $@ |
+	$STRINGTIE_EXE  --bam - 
+    )   
+fi
+
+echo $CMD 2>>  "error-$SERIES.txt" 1>> "log-$SERIES.txt"
+$CMD
