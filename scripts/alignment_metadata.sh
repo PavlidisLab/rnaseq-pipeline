7db3ab755e5ab80ed8f342b4440a3b235688cc7a
mbelmadani <manuel.belmadani@gmail.com>
2018-12-03 16:48:41 -0800

    Cleaner alignment metadata scripts.

--- scripts/alignment_metadata.sh
+++ scripts/alignment_metadata.sh
@@ -5,23 +5,23 @@ set -eu
 
 if [ "$#" -ne 1 ]; then
     ACCESSION="GSE12345"
+    echo "Usage:"
+    echo "$0 <ACCESSION>"    
     echo "Description: "
     echo "Gather and store STAR alignment logs."
-    echo "Usage:"
-    echo "$0 <ACCESSION>"
     echo "Example:"
     echo "$0 $ACCESSION"
     echo "   where $TMPDIR/$ACCESSION would hold all the temporary data."
-    exit -1
+    exit 42
 fi
 
 ### Get the header of a .gz file
-function fcat(){ echo $1 && cat $1 && echo "" ;  }; # FIXME: Is this still necessary?
+function fcat(){ echo "$1" && cat "$1" && echo "" ;  }; # FIXME: Is this still necessary?
 export -f fcat;
 
 GSE=$1 # GSE ID
 
-OUTDIR=$METADATA"/"$GSE
-mkdir -p $OUTDIR
+OUTDIR="$METADATA"/"$GSE"
+mkdir -p "$OUTDIR"
 
-find $TMPDIR/$GSE/ -name "*Log.final.out" -exec bash -c "fcat "{} \; >> $OUTDIR"/"$GSE".alignment.metadata"
+find "$TMPDIR"/"$GSE"/ -name "*Log.final.out" -exec bash -c 'fcat "$1"' _ {} \; >> "$OUTDIR"/"$GSE".alignment.metadata
