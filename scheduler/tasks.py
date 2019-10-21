6c63b983e44cda23a61d5d2ee280c5be33228676
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2019-10-21 10:51:16 -0700

    Update software versions and improve the pipeline ergonomics
    Reduce the number of available configuration options.
    
    Update STAR aligner to 2.7.3a.
    
    Update RSEM to 1.3.2.

--- scheduler/tasks.py
+++ scheduler/tasks.py
@@ -1,34 +1,39 @@
+import os
+from os.path import join
+from subprocess import check_output
+import urllib
+import tarfile
+import shlex
+
+import pandas as pd
 import luigi
 import luigi.task
 from luigi.contrib.external_program import ExternalProgramTask
-import os
-from subprocess import call, check_output
-from shutil import copyfile
-from base64 import b64encode
-import uuid
-from os.path import join
-from gemmaclient import GemmaClientAPI
-import pandas as pd
-import urllib
-from subprocess import check_call
-import gzip
-import tarfile
-from miniml_utils import extract_rnaseq_gsm
-import shlex
+
+from scheduler.miniml_utils import extract_rnaseq_gsm
+
+class WrapperTask(luigi.WrapperTask):
+    """
+    Extension to luigi.WrapperTask to inherit the dependencies outputs as well
+    as the completion condition.
+    """
+    def output(self):
+        return luigi.task.getpaths(self.requires())
+
+class FailingTask(luigi.Task):
+    def run(self):
+        raise ValueError('This task has failed.')
 
 # see luigi.cfg for details
 class rnaseq_pipeline(luigi.Config):
-    SCRIPTS = luigi.Parameter()
     ROOT_DIR = luigi.Parameter()
-    RESULTS_DIR = luigi.Parameter()
-    SCRATCH_DIR = luigi.Parameter()
     ASSEMBLIES = luigi.Parameter()
-    REQUIREMENTS = luigi.Parameter()
-    SCRIPTS = luigi.Parameter()
+
+    OUTPUT_DIR = luigi.Parameter()
     DATA = luigi.Parameter()
     QUANTDIR = luigi.Parameter()
-    TMPDIR = luigi.Parameter()
     COUNTDIR = luigi.Parameter()
+    TMPDIR = luigi.Parameter()
 
     PREFETCH_EXE = luigi.Parameter()
     PREFETCH_ARGS = luigi.Parameter()
@@ -37,9 +42,6 @@ class rnaseq_pipeline(luigi.Config):
     FASTQDUMP_EXE = luigi.Parameter()
 
     STAR_PATH = luigi.Parameter()
-    STAR_MAX_BAM_RAM = luigi.Parameter()
-    STAR_SAM_MAPPING = luigi.Parameter()
-    STAR_SHARED_MEMORY = luigi.Parameter()
 
     RSEM_DIR = luigi.Parameter()
 
@@ -51,13 +53,6 @@ class rnaseq_pipeline(luigi.Config):
     def asenv(self, attrs):
         return {attr: getattr(self, attr) for attr in attrs}
 
-class MetaTaskMixin(object):
-    """
-    Mixin that ensures that the task output is its requirements outputs.
-    """
-    def output(self):
-        return [elem.output() for elem in luigi.task.flatten(self.requires())]
-
 class PrefetchSRR(ExternalProgramTask):
     """
     Prefetch a SRR sample using prefetch
@@ -68,10 +63,8 @@ class PrefetchSRR(ExternalProgramTask):
     # behind
     retry_count = 5
 
-    resources = {'cpu': 1}
-
     def program_args(self):
-        return [rnaseq_pipeline().PREFETCH_EXE] + shlex.split(rnaseq_pipeline().PREFETCH_ARGS) #, '--ascp-path', rnaseq_pipeline().PREFETCH_ASCP_PATH, self.srr]
+        return [rnaseq_pipeline().PREFETCH_EXE] + shlex.split(rnaseq_pipeline().PREFETCH_ARGS) + [self.srr]
 
     def output(self):
         return luigi.LocalTarget(join(rnaseq_pipeline().SRA_PUBLIC_DIR, '{}.sra'.format(self.srr)))
@@ -96,36 +89,41 @@ class ExtractSRR(ExternalProgramTask):
 
     def program_args(self):
         # FIXME: this is not atomic
+        # FIXME: use fasterq-dump
         return [rnaseq_pipeline().FASTQDUMP_EXE,
-                '--gzip', '--clip',
+                '--gzip',
+                '--clip',
                 '--skip-technical',
                 '--readids',
                 '--dumpbase',
                 '--split-files',
-                '--outdir', os.path.dirname(self.output().path),
+                '--outdir', join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().DATA, self.gse, self.gsm),
                 self.input().path]
 
     def output(self):
         if self.paired_reads:
-            return [luigi.LocalTarget(join(rnaseq_pipeline().DATA, self.gse, self.gsm, self.srr + '_1.fastq.gz')),
-                    luigi.LocalTarget(join(rnaseq_pipeline().DATA, self.gse, self.gsm, self.srr + '_2.fastq.gz'))]
+            return [luigi.LocalTarget(join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().DATA, self.gse, self.gsm, self.srr + '_1.fastq.gz')),
+                    luigi.LocalTarget(join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().DATA, self.gse, self.gsm, self.srr + '_2.fastq.gz'))]
         else:
-            return [luigi.LocalTarget(join(rnaseq_pipeline().DATA, self.gse, self.gsm, self.srr + '_1.fastq.gz'))]
+            return [luigi.LocalTarget(join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().DATA, self.gse, self.gsm, self.srr + '_1.fastq.gz'))]
 
 class DownloadGSMMetadata(luigi.Task):
+    """
+    Download a GSM metadata containing the details of all related SRR runs.
+    """
     gse = luigi.Parameter()
     gsm = luigi.Parameter()
 
     def run(self):
-        # TODO: find a nicer way to query this data
         with self.output().temporary_path() as dest_filename:
+            # TODO: find a nicer way to query this data
             urllib.urlretrieve('https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&amp;db=sra&amp;rettype=runinfo&amp;term={}'.format(self.gsm),
                                filename=dest_filename)
 
     def output(self):
-        return luigi.LocalTarget(join(rnaseq_pipeline().DATA, self.gse, 'METADATA', '{}.csv'.format(self.gsm)))
+        return luigi.LocalTarget(join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().DATA, self.gse, 'METADATA', '{}.csv'.format(self.gsm)))
 
-class DownloadGSM(ExternalProgramTask):
+class DownloadGSM(luigi.Task):
     """
     Download all SRR related to a GSM
     """
@@ -167,7 +165,7 @@ class DownloadGSEMetadata(luigi.Task):
             tf.extract('{}_family.xml'.format(self.gse), os.path.dirname(self.output().path))
 
     def output(self):
-        return luigi.LocalTarget(join(rnaseq_pipeline().DATA, self.gse, 'METADATA', '{}_family.xml'.format(self.gse)))
+        return luigi.LocalTarget(join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().DATA, self.gse, 'METADATA', '{}_family.xml'.format(self.gse)))
 
 class DownloadGSE(luigi.Task):
     """
@@ -186,39 +184,44 @@ class DownloadGSE(luigi.Task):
     def output(self):
         # this needs to be satisfied first
         if self.input().exists():
-            return [DownloadGSM(self.gse, gsm).output()
-                        for gsm in extract_rnaseq_gsm(self.input().path)]
+            return [DownloadGSM(self.gse, gsm).output() for gsm in extract_rnaseq_gsm(self.input().path)]
         else:
             return []
 
-class DownloadArrayExpressSample(luigi.Task):
+class DownloadArrayExpressFastq(luigi.Task):
     experiment_id = luigi.Parameter()
     sample_id = luigi.Parameter()
-    # TODO: remove this URL
-    sample_url = luigi.Parameter()
+    fastq_url = luigi.Parameter()
 
     def run(self):
         with self.output().temporary_path() as dest_filename:
-            urllib.urlretrieve(self.sample_url, filename=dest_filename)
+            urllib.urlretrieve(self.fastq_url, filename=dest_filename)
 
     def output(self):
-        return luigi.LocalTarget(join(rnaseq_pipeline().DATA, self.experiment_id, self.sample_id, os.path.basename(self.sample_url)))
+        return luigi.LocalTarget(join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().DATA, self.experiment_id, self.sample_id, os.path.basename(self.fastq_url)))
 
-class DownloadArrayExpressExperiment(luigi.Task):
+class DownloadArrayExpressSample(WrapperTask):
+    experiment_id = luigi.Parameter()
+    sample_id = luigi.Parameter()
+    fastq_urls = luigi.ListParameter()
+
+    def requires(self):
+        return [DownloadArrayExpressFastq(self.experiment_id,  self.sample_id, fastq_url) for fastq_url in self.fastq_urls]
+
+class DownloadArrayExpressExperiment(WrapperTask):
     """
     Download all the related ArrayExpress sample to this ArrayExpress experiment.
     """
     experiment_id = luigi.Parameter()
-    def run(self):
+
+    def requires(self):
         ae_df = pd.read_csv('http://www.ebi.ac.uk/arrayexpress/files/{0}/{0}.sdrf.txt'.format(self.experiment_id), sep='\t')
-        yield [DownloadArrayExpressSample(experiment_id=self.experiment_id, sample_id=s['Comment[ENA_RUN]'], sample_url=s['Comment[FASTQ_URI]'])
-                for ix, s in ae_df.iterrows()]
+        # FIXME: properly handle the order of paired FASTQs
+        # FIXME: handle multiple-run samples
+        return [DownloadArrayExpressSample(experiment_id=self.experiment_id, sample_id=sample_id, fastq_urls=s['Comment[FASTQ_URI]'].sort_values().tolist())
+                for sample_id, s in ae_df.groupby('Comment[ENA_SAMPLE]')]
 
-    def output():
-        # FIXME: we should not run this here...
-        return [task.output() for task in next(self.run())]
-
-class DownloadSample(luigi.Task, MetaTaskMixin):
+class DownloadSample(WrapperTask):
     """
     This is a generic task for downloading an individual sample in an
     experiment.
@@ -232,7 +235,7 @@ class DownloadSample(luigi.Task, MetaTaskMixin):
         else:
             return DownloadArrayExpressSample(self.experiment_id, self.sample_id)
 
-class DownloadExperiment(luigi.Task, MetaTaskMixin):
+class DownloadExperiment(WrapperTask):
     """
     This is a generic task that detects which kind of experiment is intended to
     be downloaded so that downstream tasks can process regardless of the data
@@ -245,12 +248,42 @@ class DownloadExperiment(luigi.Task, MetaTaskMixin):
         else:
             return DownloadArrayExpressExperiment(self.experiment_id)
 
+class PrepareReference(ExternalProgramTask):
+    """
+    Prepare a STAR/RSEM reference.
+    """
+    taxon = luigi.Parameter(default='human')
+    genome_build = luigi.Parameter(default='hg38')
+    reference_build = luigi.Parameter(default='ensembl98')
+
+    resources = {'cpu': 8, 'mem': 32}
+
+    def program_args(self):
+        return [join(rnaseq_pipeline().RSEM_DIR, 'rsem-prepare-reference'),
+                join(rnaseq_pipeline().ASSEMBLIES, '{}_{}'.format(self.genome_build, self.reference_build, 'primary_assembly.gtf')),
+                '--star',
+                '--star-path', rnaseq_pipeline().STAR_PATH,
+                '-p', self.resources['cpu'],
+                join(rnaseq_pipeline().ASSEMBLIES, '{}_{}'.format(self.genome_build, self.reference_build, 'primary_assembly.fa')),
+                self.output().path]
+
+    def output(self):
+        return luigi.LocalTarget(join(rnaseq_pipeline().ASSEMBLIES, 'runtime/{}_{}/{}_0'.format(self.genome_build, self.reference_build, self.taxon)))
+
 class AlignSample(ExternalProgramTask):
+    """
+    The output of the task is a pair of isoform and gene quantification results
+    processed by STAR and RSEM.
+    """
     experiment_id = luigi.Parameter()
     sample_id = luigi.Parameter()
 
-    reference = luigi.Parameter(default='human')
-    reference_build = luigi.Parameter(default='ncbi38')
+    taxon = luigi.Parameter(default='human')
+    genome_build = luigi.Parameter(default='hg38')
+    reference_build = luigi.Parameter(default='ensembl98')
+
+    # TODO: handle strand-specific reads
+    strand_specific = luigi.BoolParameter(default=False)
 
     # this setup should allow us to run 16 parallel jobs
     resources = {'cpu': 4, 'mem': 32}
@@ -258,7 +291,7 @@ class AlignSample(ExternalProgramTask):
     def requires(self):
         # FIXME: put this in run
         self.output()[0].makedirs()
-        check_output(['mkdir', '-p', join(rnaseq_pipeline().TMPDIR, self.experiment_id, self.sample_id)])
+        check_output(['mkdir', '-p', join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().TMPDIR, '{}_{}'.format(self.genome_build, self.reference_build), self.experiment_id, self.sample_id)])
         return DownloadSample(self.experiment_id, self.sample_id)
 
     def program_args(self):
@@ -269,12 +302,19 @@ class AlignSample(ExternalProgramTask):
             '--star',
             '--star-path', rnaseq_pipeline().STAR_PATH,
             '--star-gzipped-read-file',
-            '--temporary-folder', join(rnaseq_pipeline().TMPDIR, self.experiment_id, self.sample_id)])
+            '--temporary-folder', join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().TMPDIR, '{}_{}'.format(self.genome_build, self.reference_build), self.experiment_id, self.sample_id),
+            '--keep-intermediate-files'])
 
-        if len(self.input()) != 1:
-            raise ValueError('The alignment step requires exactly one RNA-Seq dataset per sample.')
+        if self.strand_specific:
+            args.append('--strand-specific')
 
-        fastqs = [mate.path for mate in self.input()[0]]
+        sample_runs = self.input()
+
+        # FIXME: we should require at least one and choose one among them
+        if len(sample_runs) != 1:
+            raise ValueError('The alignment step requires exactly one RNA-Seq run per sample.')
+
+        fastqs = [mate.path for mate in sample_runs[0]]
 
         if len(fastqs) == 1:
             pass # single-ended (default)
@@ -285,91 +325,111 @@ class AlignSample(ExternalProgramTask):
 
         args.extend(fastqs)
 
-        # STAR reference
-        args.append('/space/grp/Pipelines/rnaseq-pipeline/Assemblies/runtime/{0}_ref{1}/{0}_0'.format(self.reference, self.reference_build))
+        # reference for alignments and quantifications
+        args.append(join(rnaseq_pipeline().ASSEMBLIES, 'runtime/{}_{}/{}_0'.format(self.genome_build, self.reference_build, self.taxon)))
 
-        # output
-        args.append(join(rnaseq_pipeline().QUANTDIR, self.experiment_id, self.sample_id))
-
-        args.extend([
-            '--star-shared-memory', rnaseq_pipeline().STAR_SHARED_MEMORY,
-            '--keep-intermediate-files'])
+        # output prefix
+        args.append(join(os.path.dirname(self.output()[0].path), self.sample_id))
 
         return args
 
     def output(self):
-        destdir = join(rnaseq_pipeline().QUANTDIR, self.experiment_id)
+        destdir = join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().QUANTDIR, '{}_{}'.format(self.genome_build, self.reference_build), self.experiment_id)
         return [luigi.LocalTarget(join(destdir, '{}.isoforms.results'.format(self.sample_id))),
                 luigi.LocalTarget(join(destdir, '{}.genes.results'.format(self.sample_id)))]
 
 class AlignExperiment(luigi.Task):
+    """
+    Align all the samples in a given experiment.
+
+    The output is one sample alignment output per sample contained in the
+    experiment.
+    """
     experiment_id = luigi.Parameter()
 
-    reference = luigi.Parameter(default='human')
-    reference_build = luigi.Parameter(default='ncbi38')
+    taxon = luigi.Parameter(default='human')
+    genome_build = luigi.Parameter(default='hg38')
+    reference_build = luigi.Parameter(default='ensembl98')
 
     def requires(self):
         return DownloadExperiment(self.experiment_id)
 
     def run(self):
         # FIXME: do this better
-        yield [AlignSample(self.experiment_id, os.path.basename(os.path.dirname(sample[0][0].path)), self.reference, self.reference_build)
-                for sample in self.input()]
-
-    def output(self):
-        return [AlignSample(self.experiment_id, os.path.basename(os.path.dirname(sample[0][0].path)), self.reference, self.reference_build).output()
+        yield [AlignSample(self.experiment_id, os.path.basename(os.path.dirname(sample[0][0].path)), self.taxon, self.genome_build, self.reference_build)
                     for sample in self.input()]
 
-class CountExperiment(ExternalProgramTask):
+    def output(self):
+        return [AlignSample(self.experiment_id, os.path.basename(os.path.dirname(sample[0][0].path)), self.taxon, self.genome_build, self.reference_build).output()
+                    for sample in self.input()]
+
+class CountExperiment(luigi.Task):
+    """
+    Combine the RSEM quantifications results from all the samples in a given
+    experiment.
+
+    The output is two matrices: counts and FPKM.
+    """
     experiment_id = luigi.Parameter()
 
-    reference = luigi.Parameter(default='human')
-    reference_build = luigi.Parameter(default='ncbi38')
-
-    scope = luigi.Parameter(default='genes')
+    taxon = luigi.Parameter(default='human')
+    genome_build = luigi.Parameter(default='hg38')
+    reference_build = luigi.Parameter(default='ensembl98')
 
     resources = {'cpu': 1}
 
     def requires(self):
-        return AlignExperiment(self.experiment_id, self.reference, self.reference_build)
+        return AlignExperiment(self.experiment_id, self.taxon, self.genome_build, self.reference_build)
 
-    def program_args(self):
-        return [join(rnaseq_pipeline().ROOT_DIR, 'Pipelines/rsem/rsem_count.sh'), join(rnaseq_pipeline().QUANTDIR, self.experiment_id), self.scope]
+    def run(self):
+        # FIXME: this is a hack
+        keys = [align_task.sample_id for align_task in next(self.requires().run())]
+        counts_df = pd.concat([pd.read_csv(gene.path, sep='\t', index_col=0).expected_count
+            for isoform, gene in self.input()], keys=keys, axis=1)
+        fpkm_df = pd.concat([pd.read_csv(gene.path, sep='\t', index_col=0).FPKM
+            for isoform, gene in self.input()], keys=keys, axis=1)
 
-    def program_environment(self):
-        return rnaseq_pipeline().asenv(['RSEM_DIR'])
+        with self.output()[0].open('w') as f:
+            counts_df.to_csv(f, sep='\t')
+
+        with self.output()[1].open('w') as f:
+            fpkm_df.to_csv(f, sep='\t')
 
     def output(self):
-        destdir = join(rnaseq_pipeline().QUANTDIR, self.experiment_id)
-        return [luigi.LocalTarget(join(destdir, 'countMatrix.{}'.format(self.scope))),
-                luigi.LocalTarget(join(destdir, 'tpmMatrix.{}'.format(self.scope))),
-                luigi.LocalTarget(join(destdir, 'fpkmMatrix.{}'.format(self.scope)))]
+        destdir = join(rnaseq_pipeline().OUTPUT_DIR, rnaseq_pipeline().COUNTDIR, '{}_{}'.format(self.genome_build, self.reference_build))
+        return [luigi.LocalTarget(join(destdir, '{}_counts.genes'.format(self.experiment_id))),
+                luigi.LocalTarget(join(destdir, '{}_fpkm.genes'.format(self.experiment_id)))]
 
 class SubmitExperimentToGemma(ExternalProgramTask):
     """
-    Load experiment into Gemma.
+    Submit an experiment to Gemma.
+
+    This will trigger the whole pipeline.
+
+    The output is a confirmation that the data is effectively in Gemma.
     """
     experiment_id = luigi.Parameter()
 
-    reference = luigi.Parameter(default='human')
-    reference_build = luigi.Parameter(default='ncbi38')
+    taxon = luigi.Parameter(default='human')
+    genome_build = luigi.Parameter(default='hg38')
+    reference_build = luigi.Parameter(default='ensembl98')
 
     def requires(self):
-        return CountExperiment(self.experiment_id, self.reference, self.reference_build)
+        return CountExperiment(self.experiment_id, self.taxon, self.genome_build, self.reference_build)
 
     def program_environment(self):
         return rnaseq_pipeline().asenv(['GEMMA_LIB', 'JAVA_HOME', 'JAVA_OPTS'])
 
     def program_args(self):
-        count, tpm, fpkm = self.input()
+        count, fpkm = self.input()
         return [rnaseq_pipeline().GEMMACLI, 'rnaseqDataAdd',
                 '-u', os.getenv('GEMMAUSERNAME'),
                 '-p', os.getenv('GEMMAPASSWORD'),
                 '-e', self.experiment_id,
-                '-a', 'Generic_{}_ensemblIds'.format(self.reference),
+                '-a', 'Generic_{}_ensemblIds'.format(self.taxon),
                 '-count', count.path,
                 '-rpkm', fpkm.path]
 
     def output(self):
-        # check if data is in Gemma
-        pass
+        # TODO: check if data is in Gemma
+        return []
