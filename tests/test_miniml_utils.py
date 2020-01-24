26c23c9bc20dbf6c09ce0e3d73d1dc326937d6cc
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2020-01-24 10:22:16 -0800

    Fix test for miniml_utils module

--- tests/test_miniml_utils.py
+++ tests/test_miniml_utils.py
@@ -1,7 +1,9 @@
-from scheduler.miniml_utils import *
+from rnaseq_pipeline.miniml_utils import *
 
 def test_extract_rnaseq_gsm():
     collect_geo_samples_with_rnaseq_data('tests/data/GSE100007_family.xml')
+    collect_geo_samples_with_rnaseq_data('tests/data/GSM69846.xml')
 
 def test_collect_geo_samples_info():
     collect_geo_samples_info('tests/data/GSE100007_family.xml')
+    collect_geo_samples_info('tests/data/GSM69846.xml')
