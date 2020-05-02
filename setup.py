217ceafb6960c6b4510dc13a2a27ef67a5603590
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2020-05-02 09:24:55 -0700

    Install MultiQC directly from pip
    The Conda package for MultiQC conflicts for some obscure reasons and forces us
    to use Python 3.5 whereas the pip package is fine.

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name='rnaseq_pipeline',
       packages=find_packages(),
-      install_requires=['luigi', 'bioluigi', 'PyYAML', 'requests', 'pandas'],
+      install_requires=['luigi', 'bioluigi', 'PyYAML', 'requests', 'pandas', 'multiqc'],
       dependency_links=['git+https://github.com/PavlidisLab/bioluigi.git#egg=bioluigi-0.0.0'])
