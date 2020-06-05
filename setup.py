c171765b956d39803e5c4ca93762c82c4742c337
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2020-06-05 15:29:08 -0700

    Add conda-forge to deps and fix more versions

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name='rnaseq_pipeline',
       packages=find_packages(),
-      install_requires=['luigi==2.8.13', 'bioluigi', 'PyYAML', 'requests', 'pandas', 'multiqc'],
+      install_requires=['luigi==2.8.13', 'bioluigi', 'PyYAML', 'requests', 'pandas'],
       dependency_links=['git+https://github.com/PavlidisLab/bioluigi.git#egg=bioluigi-0.0.0'])
