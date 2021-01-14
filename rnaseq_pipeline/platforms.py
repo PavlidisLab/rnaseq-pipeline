e80580380da1bf17bc35a31345413609cef5042a
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-01-14 11:27:17 -0800

    Add support for BGI platform
    Add reverse_3prime_adapter in cutadapt.TrimPairedReads to handle different
    adapters for each mate.

--- rnaseq_pipeline/platforms.py
+++ rnaseq_pipeline/platforms.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 from bioluigi.tasks import cutadapt
 
 class Platform:
-    def __init__(self, instrument=None):
+    def __init__(self, instrument):
         self.instrument = instrument
 
     @abstractmethod
@@ -14,35 +14,43 @@ class Platform:
         pass
 
 class BgiPlatform(Platform):
-    @staticmethod
-    def get_trim_single_end_reads_task(r1, dest, **kwargs):
+    # http://seqanswers.com/forums/showthread.php?t=87647 led to a document
+    # from BGI mentioning to the following sequences:
+    FORWARD_FILTER = 'AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA'
+    REVERSE_FILTER = 'AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG'
+
+    def get_trim_single_end_reads_task(self, r1, dest, **kwargs):
         return cutadapt.TrimReads(
                 r1,
                 dest,
-                adapter_3prime='AGATCGGAAGAGC',
+                adapter_3prime=BgiPlatform.FORWARD_FILTER,
                 **kwargs)
 
-    @staticmethod
-    def get_trim_paired_reads_task(r1,r2, r1_dest, r2_dest, **kwargs):
+    def get_trim_paired_reads_task(self, r1, r2, r1_dest, r2_dest, **kwargs):
         return cutadapt.TrimPairedReads(
                 r1, r2,
                 r1_dest, r2_dest,
-                adapter_3prime='AGATCGGAAGAGC',
+                adapter_3prime=BgiPlatform.FORWARD_FILTER,
+                reverse_adapter_3prime=BgiPlatform.REVERSE_FILTER,
                 **kwargs)
+        raise NotImplementedError('Trimming paired reads is not supported for this platform.')
 
 class IlluminaPlatform(Platform):
+    UNIVERSAL_ADAPTER = 'AGATCGGAAGAGC'
+
     @staticmethod
     def get_trim_single_end_reads_task(r1, dest, **kwargs):
         return cutadapt.TrimReads(
                 r1,
                 dest,
-                adapter_3prime='AGATCGGAAGAGC',
+                adapter_3prime=IlluminaPlatform.UNIVERSAL_ADAPTER,
                 **kwargs)
 
     @staticmethod
-    def get_trim_paired_reads_task(r1,r2, r1_dest, r2_dest, **kwargs):
+    def get_trim_paired_reads_task(r1, r2, r1_dest, r2_dest, **kwargs):
         return cutadapt.TrimPairedReads(
                 r1, r2,
                 r1_dest, r2_dest,
-                adapter_3prime='AGATCGGAAGAGC',
+                adapter_3prime=IlluminaPlatform.UNIVERSAL_ADAPTER,
+                reverse_adapter_3prime=IlluminaPlatform.UNIVERSAL_ADAPTER,
                 **kwargs)
--- tests/test_platforms.py
+++ tests/test_platforms.py
@@ -1,12 +1,18 @@
 from rnaseq_pipeline.platforms import BgiPlatform, IlluminaPlatform
 
 def test_bgi_platform_trim_single_end_reads():
-    task = BgiPlatform().get_trim_single_end_reads_task('r1', 'r1_dest')
+    task = BgiPlatform('BGISEQ-500').get_trim_single_end_reads_task('r1', 'r1_dest')
+    assert 'AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA' in task.program_args()
+
+def test_bgi_platform_trim_paired_end_reads():
+    task = BgiPlatform('BGISEQ-500').get_trim_paired_reads_task('r1', 'r2', 'r1_dest', 'r2_dest')
+    assert 'AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA' in task.program_args()
+    assert 'AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG' in task.program_args()
 
 def test_illumin_platform_trim_single_end_reads():
-    task = IlluminaPlatform().get_trim_single_end_reads_task('r1', 'r1_dest')
+    task = IlluminaPlatform('HiSeq 2500').get_trim_single_end_reads_task('r1', 'r1_dest')
     assert 'AGATCGGAAGAGC' in task.program_args()
 
 def test_illumin_platform_trim_paired_reads():
-    task = IlluminaPlatform().get_trim_paired_reads_task('r1', 'r2', 'r1_dest', 'r2_dest')
+    task = IlluminaPlatform('HiSeq 2500').get_trim_paired_reads_task('r1', 'r2', 'r1_dest', 'r2_dest')
     assert 'AGATCGGAAGAGC' in task.program_args()
