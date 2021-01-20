1d4a078ce91e62de31b9cbcd664d65684894c23e
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-01-19 17:52:29 -0800

    Use latest Luigi release

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup, find_packages
 
 setup(name='rnaseq_pipeline',
       packages=find_packages(),
-      install_requires=['luigi==2.8.13', 'bioluigi', 'PyYAML', 'requests', 'pandas', 'Flask'])
+      install_requires=['luigi', 'bioluigi', 'PyYAML', 'requests', 'pandas', 'Flask'])
