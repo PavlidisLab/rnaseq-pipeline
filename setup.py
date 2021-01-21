3c6f7ac7e64cd137830caf9815efe63dbff4fcc9
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-01-21 10:32:02 -0800

    Bump major version to 2.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rnaseq_pipeline',
-      version='1.3.2',
+      version='2.0.0',
       description='RNA-Seq pipeline for the Pavlidis Lab',
       long_description='file: README.md',
       author='Guillaume Poirier-Morency',
