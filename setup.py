1e4948cad07fac9519c4bbb0634e058dad61363e
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-01-21 10:33:07 -0800

    Add packaging metadata

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='rnaseq_pipeline',
+      version='1.3.2',
+      description='RNA-Seq pipeline for the Pavlidis Lab',
+      long_description='file: README.md',
+      author='Guillaume Poirier-Morency',
+      author_email='poirigui@msl.ubc.ca',
       packages=find_packages(),
       install_requires=['luigi', 'bioluigi', 'PyYAML', 'requests', 'pandas', 'Flask'])
