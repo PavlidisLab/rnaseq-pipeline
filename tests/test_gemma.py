3d576748b5e0581aff9d2425f88f914300dc179e
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2021-08-13 10:59:59 -0700

    Use GSE110256 in tests
    It is a public dataset with batch information.

--- tests/test_gemma.py
+++ tests/test_gemma.py
@@ -2,5 +2,5 @@ from rnaseq_pipeline.gemma import *
 
 def test_gemma_api():
     gemma_api = GemmaApi()
-    gemma_api.datasets('GSE173137')
-    gemma_api.samples('GSE173137')
+    gemma_api.datasets('GSE110256')
+    gemma_api.samples('GSE110256')
