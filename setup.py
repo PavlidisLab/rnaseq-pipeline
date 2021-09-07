fb0958bc659af38c56f7feb11ef2db2eed3fe6e9
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-09-07 12:12:15 -0700

    Add license and classifier in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(name='rnaseq_pipeline',
       version='2.1.1',
       description='RNA-Seq pipeline for the Pavlidis Lab',
+      license='Public Domain',
       long_description='file: README.md',
       author='Guillaume Poirier-Morency',
       author_email='poirigui@msl.ubc.ca',
+      classifiers=['License :: Public Domain'],
       packages=find_packages(),
       install_requires=['luigi', 'bioluigi', 'PyYAML', 'requests', 'pandas==1.1.4'],
       extras_require={
