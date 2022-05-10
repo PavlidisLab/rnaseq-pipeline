e019ceabae979e3054ed7b4a54b6c3d4439261d0
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-05-10 10:02:21 -0700

    Bump patch version to 2.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rnaseq_pipeline',
-      version='2.1.5',
+      version='2.1.6',
       description='RNA-Seq pipeline for the Pavlidis Lab',
       license='Public Domain',
       long_description='file: README.md',
