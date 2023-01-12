98ac2bd19375a7af8a6efa2b27f350e529350ba2
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-01-12 10:50:05 -0800

    sra: Add an extra check for empty runinfo when the file solely contains a header

--- rnaseq_pipeline/sources/sra.py
+++ rnaseq_pipeline/sources/sra.py
@@ -97,7 +97,7 @@ def retrieve_runinfo(sra_accession):
     """Retrieve a SRA runinfo using search and efetch utilities"""
     esearch_proc = Popen(['esearch', '-db', 'sra', '-query', sra_accession], stdout=PIPE)
     runinfo_data = check_output(['efetch', '-format', 'runinfo'], universal_newlines=True, stdin=esearch_proc.stdout)
-    if not runinfo_data.strip():
+    if not runinfo_data.strip() or (len(runinfo_data.splitlines()) == 1 and runinfo_data[:3] == 'Run'):
         raise EmptyRunInfoError(f"Runinfo for {sra_accession} is empty.")
     return runinfo_data
 
