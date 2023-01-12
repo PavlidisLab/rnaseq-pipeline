78af76e161bf9fdf55337f3e8d2369b8c1ff3835
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-01-12 12:00:54 -0800

    gsheet: Use the local server for the authentication flow

--- rnaseq_pipeline/gsheet.py
+++ rnaseq_pipeline/gsheet.py
@@ -34,7 +34,7 @@ def _authenticate():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_console()
+            creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open(token_path, 'wb') as token:
             pickle.dump(creds, token)
