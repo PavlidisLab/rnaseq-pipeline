2c89fa6bbd91805b65e45d0d8afc2c4e8817bbd3
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-01-17 12:17:24 -0800

    Update patch version to 2.1.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rnaseq_pipeline',
-      version='2.1.9',
+      version='2.1.10',
       description='RNA-Seq pipeline for the Pavlidis Lab',
       license='Public Domain',
       long_description='file: README.md',
