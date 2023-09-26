c762d141dae7a904df681652209af833bbee95e2
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-09-26 12:28:05 -0700

    Indicate which labelling is likely problematic when DumpSraRun fails to complete

--- rnaseq_pipeline/sources/sra.py
+++ rnaseq_pipeline/sources/sra.py
@@ -81,7 +81,8 @@ class DumpSraRun(luigi.Task):
         yield sratoolkit.FastqDump(self.input().path,
                                    join(cfg.OUTPUT_DIR, cfg.DATA, 'sra', self.srx))
         if not self.complete():
-            raise RuntimeError(f'{repr(self)} was not completed after successful fastq-dump execution. Is it possible the SRA run is mislabelled as single-end?')
+            labelling = 'paired' if self.paired_reads else 'single-end'
+            raise RuntimeError(f'{repr(self)} was not completed after successful fastq-dump execution. Is it possible the SRA run is mislabelled as {labelling}?')
 
     def output(self):
         output_dir = join(cfg.OUTPUT_DIR, cfg.DATA, 'sra', self.srx)
